8e45672a9bab190a6c8c631966f72b1c3bd04a9e
frank <ipfung@gmail.com>
2022-08-05 14:18:12 +0100

    updated timezone to HK.

--- config/app.php
+++ config/app.php
@@ -67,7 +67,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Hong_Kong',
+//    'timezone' => 'Europe/London',
 
     /*
     |--------------------------------------------------------------------------
