8b3bf929f5d468d9ddd3cb4c5e8473cc58b2a60b
frank <ipfung@gmail.com>
2022-10-21 04:15:14 +0100

    updated missing for PackageApproved email where we need json_decoded order_description.

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
 
+    public function getDescriptionAttribute() {
+        // string to json.
+        return json_decode($this->order_description);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo('App\Models\Order');
+    }
+//
+//    public function payments()
+//    {
+//        return $this->hasMany('App\Models\Payments', 'order_id', 'order_id' );
+//    }
 }
