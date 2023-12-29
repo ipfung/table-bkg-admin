3738515542aa680a6c4c8096d4a64f67862a3ed5
frank <ipfung@gmail.com>
2023-12-29 21:12:40 +0000

    fixed in php we have to use object's property name(customerBookings) instead of json name(customer_bookings).

--- resources/views/exports/trainer-commission.blade.php
+++ resources/views/exports/trainer-commission.blade.php
@@ -11,47 +11,47 @@
 </table>
 
     <table class="table table-striped table-hover">
-        
+
         <thead class="thead-dark">
         <tr>
             <th style="width:100px;"><b>{{ __("Trainer") }}</b></th>
             <th style="width:100px;"><b>{{ __("Lesson Date") }}</b></th>
-            <th style="width:100px;"><b>{{ __("Duration") }}</b></th>            
+            <th style="width:100px;"><b>{{ __("Duration") }}</b></th>
             <th style="width:80px;"><b>{{ __("Student") }}</b></th>
             <th style="width:100px;"><b>{{ __("Commission") }}</b></th>
         </tr>
-      
+
         </thead>
         <tbody>
             @php
-                $total = 0;    
+                $total = 0;
                 $total_unpaid = 0;
             @endphp
         @foreach($trainer_commissions as $trainer_commission)
         <tr>
             <td>{{ $trainer_commission->user->name }}</td>
-            <td>{{ $trainer_commission->start_time }}</td>            
+            <td>{{ $trainer_commission->start_time }}</td>
             <td>{{ $trainer_commission->simpleduration }}</td>
-       {{--      <td>{{ $trainer_commission->paid_amount }}</td>    --}}    
-            <td> 
-                 <table>                
-                    @foreach($trainer_commission->customer_bookings as $student  )                     
+       {{--      <td>{{ $trainer_commission->paid_amount }}</td>    --}}
+            <td>
+                 <table>
+                    @foreach($trainer_commission->customerBookings as $student  )
                         <tr>
-                            <td>{{$student->customer->name}}</td>   
-                        </tr>       
+                            <td>{{$student->customer->name}}</td>
+                        </tr>
                     @endforeach
-                </table> 
+                </table>
             </td>
         </tr>
         @php
             /* if ($order->payment_status==2002) {//paid
-                $total = $total + $order->order_total;    
+                $total = $total + $order->order_total;
             } else {
-                $total_unpaid = $total_unpaid + $order->order_total; 
+                $total_unpaid = $total_unpaid + $order->order_total;
             } */
         @endphp
         @endforeach
-        
+
   {{--   <tr>
         <td colspan="3" align="right" ><strong>Total:</strong></td>
         <td align="right" ><strong>HK${{$total}}</strong></td>
@@ -60,4 +60,3 @@
     </tr> --}}
         </tbody>
     </table>
-   
